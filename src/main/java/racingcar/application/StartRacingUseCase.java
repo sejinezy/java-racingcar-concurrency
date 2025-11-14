4bf3ea24d658749dda4f83287cfc2bbcb299b6eb
sejin <psj8880@naver.com>
2025-11-14 18:05:21 +0900

    feat(StartRacingUseCase): 멀티스레드를 사용하도록 구현
    - Executors.newFixedThreadPool(4) : 스레드 4개 사용하도록 설정

--- src/main/java/racingcar/application/StartRacingUseCase.java
+++ src/main/java/racingcar/application/StartRacingUseCase.java
@@ -1,12 +1,15 @@
 package racingcar.application;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import racingcar.application.dto.RoundResult;
 import racingcar.domain.Attempts;
+import racingcar.domain.ConcurrentTurnRunner;
 import racingcar.domain.ParticipatingCars;
 import racingcar.service.GameEngine;
 import racingcar.domain.port.PickRandomValue;
-import racingcar.domain.RacingTurnRunner;
 
 public class StartRacingUseCase {
 
@@ -18,12 +21,17 @@ public class StartRacingUseCase {
         this.gameEngine = gameEngine;
     }
 
-    public List<RoundResult> execute(List<String> carNames, String attemptsInput) {
+    public List<RoundResult> execute(List<String> carNames, String attemptsInput)
+            throws InterruptedException, ExecutionException {
         ParticipatingCars participatingCars = new ParticipatingCars(carNames);
-        RacingTurnRunner racingGame = new RacingTurnRunner(participatingCars,pickRandomValue);
+
+        ExecutorService es = Executors.newFixedThreadPool(4);
+
+        ConcurrentTurnRunner concurrentTurnRunner = new ConcurrentTurnRunner(pickRandomValue, participatingCars, es);
+
         Attempts attempts = new Attempts(attemptsInput);
 
-        return gameEngine.runAll(attempts, racingGame);
+        return gameEngine.runAll(attempts, concurrentTurnRunner);
     }
 
     public List<String> extractWinners(List<RoundResult> allResult) {
