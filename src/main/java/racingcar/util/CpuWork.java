a03e9e8b8fec1fa0ba5e93648bae094127bdc697
sejin <psj8880@naver.com>
2025-11-15 21:37:54 +0900

    feat(CpuWork): CPU를 사용하기 위한 더미 연산 구현
    - 멀티스레드와 싱글스레드의 성능 테스트를 비교하고 싶은데, 멀티스레드로 동작하는 Callable 작업이 너무 간단해서 오히려 실글 스레드의 성능이 더 좋고, 멀티스레드는 오버헤드가 발생하는 사태가 발생함.
    - 멀티 스레드 성능이 싱글 스레드 성능보다 더 좋아지는 지점을 찾기 위해 cpu 연산을 추가함.
    - 사실상 자동차 경주 기능과는 전혀 관련 없는 연산임. 오로지 성능 테스트를 위한 더미 연산일 뿐이다.

---
+++ src/main/java/racingcar/util/CpuWork.java
@@ -0,0 +1,18 @@
+package racingcar.util;
+
+public class CpuWork {
+
+    private static volatile long BLACK_HOLE = 0;
+
+    public static void heavyWork() {
+        long x = 0;
+        for (int i = 0; i < 10_000; i++) {
+            x += i;
+        }
+        BLACK_HOLE = x;
+    }
+
+    public static long getBlackHole() {
+        return BLACK_HOLE;
+    }
+}
