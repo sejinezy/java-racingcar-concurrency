7495d16e58754aa72722d88400ac409ae16fefdc
sejin <psj8880@naver.com>
2025-11-16 20:36:09 +0900

    feat(SingleThreadTurnRunner): 싱글 스레드 환경에 StartegyAi 적용
    - pickRandomValue 대신 StrategyAi 주입
    - 베스트 전략을 계산한 후 그 전략을 선택해 전진

--- src/main/java/racingcar/domain/singlethread/SingleThreadTurnRunner.java
+++ src/main/java/racingcar/domain/singlethread/SingleThreadTurnRunner.java
@@ -6,40 +6,38 @@ import racingcar.application.dto.RoundResult;
 import racingcar.domain.Car;
 import racingcar.domain.CarPosition;
 import racingcar.domain.ParticipatingCars;
-import racingcar.domain.port.PickRandomValue;
-import racingcar.util.CpuWork;
+import racingcar.domain.strategy.Strategy;
+import racingcar.domain.strategy.StrategyAi;
 
 public class SingleThreadTurnRunner {
 
     private static final int MOVE_THRESHOLD = 4;
 
-    private final ParticipatingCars participatingCars;
-    private final PickRandomValue pickRandomValue;
+    private final ParticipatingCars cars;
+    private final StrategyAi ai;
 
-    public SingleThreadTurnRunner(ParticipatingCars participatingCars, PickRandomValue pickRandomValue) {
-        this.participatingCars = participatingCars;
-        this.pickRandomValue = pickRandomValue;
+    public SingleThreadTurnRunner(ParticipatingCars cars, StrategyAi ai) {
+        this.cars = cars;
+        this.ai = ai;
     }
 
-    public RoundResult runOneTime() {
-        for (Car car : participatingCars.getCars()) {
-            operate(car);
+
+    public RoundResult runOneTime(int remainTurns) {
+        for (Car car : cars.getCars()) {
+            operate(car,remainTurns);
         }
         return resultOneTime();
     }
 
-    private void operate(Car car) {
-        int randomValue = pickRandomValue.pickRandomNumber();
-        CpuWork.heavyWork(); // 더미 연산
-        if (randomValue >= MOVE_THRESHOLD) {
-            car.moveForward();
-        }
+    private void operate(Car car,int remainTurns) {
+        Strategy strategy = ai.decideBestStrategy(car, cars.getCars(), remainTurns);
+        car.moveAccordingTo(strategy);
     }
 
     private RoundResult resultOneTime() {
         List<CarPosition> gameResult = new ArrayList<>();
 
-        for (Car car : participatingCars.getCars()) {
+        for (Car car : cars.getCars()) {
             CarPosition carPosition = new CarPosition(car.getName(), car.getPosition());
             gameResult.add(carPosition);
         }
