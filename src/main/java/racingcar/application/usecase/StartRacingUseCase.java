798d640ae725a8c2270f92018112cad37514b5b8
sejin <psj8880@naver.com>
2025-11-17 16:00:38 +0900

    refactor(StartRacingUseCase): 매직 넘버 상수 처리 , 인터페이스 주입
    - 구현체 대신 인터페이스를 주입 받는다.
    - 구현체를 갈아 끼울 때 코드를 수정하지 않아도 된다.

--- src/main/java/racingcar/application/usecase/StartRacingUseCase.java
+++ src/main/java/racingcar/application/usecase/StartRacingUseCase.java
@@ -1,35 +1,35 @@
-package racingcar.application;
+package racingcar.application.usecase;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import racingcar.application.engine.GameEngine;
 import racingcar.domain.RoundResult;
 import racingcar.domain.Attempts;
-import racingcar.domain.multithread.ConcurrentTurnRunner;
+import racingcar.application.turn.TurnRunner;
+import racingcar.application.turn.ConcurrentTurnRunner;
 import racingcar.domain.ParticipatingCars;
 import racingcar.domain.strategy.StrategyAi;
-import racingcar.application.multithread.MultiThreadGameEngine;
 
 public class StartRacingUseCase {
 
-    private final StrategyAi ai;
-    private final MultiThreadGameEngine gameEngine;
+    private final static int THREADS = 8;
 
-    public StartRacingUseCase(StrategyAi ai, MultiThreadGameEngine gameEngine) {
+    private final StrategyAi ai;
+    private final GameEngine gameEngine;
+
+    public StartRacingUseCase(StrategyAi ai, GameEngine gameEngine) {
         this.ai = ai;
         this.gameEngine = gameEngine;
     }
 
-
     public List<RoundResult> execute(List<String> carNames, String attemptsInput)
             throws InterruptedException, ExecutionException {
+
         ParticipatingCars participatingCars = new ParticipatingCars(carNames);
-
-        ExecutorService es = Executors.newFixedThreadPool(8);
-
-        ConcurrentTurnRunner concurrentTurnRunner = new ConcurrentTurnRunner(ai, participatingCars, es);
-
+        ExecutorService es = Executors.newFixedThreadPool(THREADS);
+        TurnRunner concurrentTurnRunner = new ConcurrentTurnRunner(ai, participatingCars, es);
         Attempts attempts = new Attempts(attemptsInput);
 
         return gameEngine.runAll(attempts, concurrentTurnRunner);
@@ -37,6 +37,5 @@ public class StartRacingUseCase {
 
     public List<String> extractWinners(List<RoundResult> allResult) {
         return gameEngine.getWinner(allResult.getLast());
-
     }
 }
