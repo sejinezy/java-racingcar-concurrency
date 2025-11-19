298d102423d2a8bd35db1d810cc7557cfd0b0370
sejin <psj8880@naver.com>
2025-11-19 14:15:08 +0900

    refactor(Application): 예외 처리 + 우아한 종료 구현
    - 사용자 입력 오류는 회복 불가능한 에러가 아닌 단순한 실수이기 때문에 "IllegalArgumentException -> catch -> 에러 메시지 출력 -> 종료" 패턴 적용
    - 개발자 실수 / 시스템 오류(ExecutionException, RuntimeException, NullPointException 등)는 그대로 터뜨린다. 잡아서 숨기면 문제 파악이 어려워진다.
    - finally에서 ExecutorService 우아한 종료 : 무조건 스레드풀 종료는 수행된다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,8 @@
 package racingcar;
 
-
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import racingcar.application.engine.GameEngine;
 import racingcar.application.usecase.StartRacingUseCase;
 import racingcar.ui.controller.GameController;
@@ -11,16 +12,49 @@ import racingcar.ui.view.InputView;
 import racingcar.ui.view.OutputView;
 
 public class Application {
+    private static final String SHUTDOWN_STAGE1_ATTEMPT = "[POOL] (1/3) 정상 종료 시도";
+    private static final String SHUTDOWN_STAGE2_FORCED = "[POOL] (2/3) 정상 종료 실패 -> 강제 종료 시도";
+    private static final String SHUTDOWN_STAGE3_UNTERMINATED = "[POOL] (3/3) 서비스가 종료되지 않았습니다.";
+    private static final int TIME_OUT = 10;
     private static final int SIMULATION_COUNT = 2000;
+    private final static int THREADS = 8;
+
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        StrategyAi ai = new StrategyAi(SIMULATION_COUNT);
-        GameEngine gameEngine = new MultiThreadGameEngine();
-        OutputView outputView = new OutputView();
-        StartRacingUseCase startRacingUseCase = new StartRacingUseCase(ai, gameEngine);
-        GameController gameController = new GameController(inputView, startRacingUseCase, outputView);
+        ExecutorService es = Executors.newFixedThreadPool(THREADS);
 
-        gameController.run();
+        try {
+            InputView inputView = new InputView();
+            StrategyAi ai = new StrategyAi(SIMULATION_COUNT);
+            GameEngine gameEngine = new MultiThreadGameEngine();
+            OutputView outputView = new OutputView();
+            StartRacingUseCase startRacingUseCase = new StartRacingUseCase(ai, gameEngine, es);
+            GameController gameController = new GameController(inputView, startRacingUseCase, outputView);
+
+            gameController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            shutdownAndAwaitTermination(es);
+        }
+    }
+
+    private static void shutdownAndAwaitTermination(ExecutorService es){
+        es.shutdown();
+
+        try {
+            System.out.println(SHUTDOWN_STAGE1_ATTEMPT);
+            if (!es.awaitTermination(TIME_OUT, TimeUnit.SECONDS)) {
+                System.out.println(SHUTDOWN_STAGE2_FORCED);
+                es.shutdownNow();
+
+                if (!es.awaitTermination(TIME_OUT, TimeUnit.SECONDS)) {
+                    System.out.println(SHUTDOWN_STAGE3_UNTERMINATED);
+                }
+            }
+        } catch (InterruptedException ex){
+            es.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 }
