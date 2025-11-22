43011a254d38338e32206818e0ca34a52e0aa055
sejin <psj8880@naver.com>
2025-11-22 12:19:06 +0900

    test(ConcurrentTurnRunnerTest): 테스트 작성
    - 어떤 전략이 선택됐는지, 누가 이겼는 지는 신경쓰지 않는다.
    - 모든 Car에 대해 결과가 하나씩 만들어지는 지 검증한다.

---
+++ src/test/java/racingcar/application/turn/ConcurrentTurnRunnerTest.java
@@ -0,0 +1,81 @@
+package racingcar.application.turn;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.ParticipatingCars;
+import racingcar.domain.RoundResult;
+import racingcar.domain.strategy.DefaultWinRateCalculator;
+import racingcar.domain.strategy.Strategy;
+import racingcar.domain.strategy.StrategyAi;
+import racingcar.domain.strategy.WinRateCalculator;
+
+class ConcurrentTurnRunnerTest {
+
+    static class FixedStrategyAi extends StrategyAi {
+
+
+        public FixedStrategyAi(WinRateCalculator winRateCalculator) {
+            super(winRateCalculator);
+        }
+
+        @Override
+        public Strategy decideBestStrategy(Car me, List<Car> all, int remainTurns) {
+            return Strategy.NORMAL;
+        }
+    }
+
+    private final ExecutorService es = Executors.newFixedThreadPool(4);
+
+    @AfterEach
+    void shutdown() {
+        es.shutdown();
+    }
+
+    @Test
+    void run_한_턴_실행시_참여한_자동차_수만큼_결과를_반환한다() {
+        ParticipatingCars cars = new ParticipatingCars(List.of("pobi", "woni", "jun"));
+        Car pobi = cars.getCars().getFirst();
+        Car woni = cars.getCars().get(1);
+        Car jun = cars.getCars().get(2);
+        FixedStrategyAi ai = new FixedStrategyAi(new DefaultWinRateCalculator(10));
+        ConcurrentTurnRunner runner = new ConcurrentTurnRunner(ai, cars, es);
+        int remainTurns = 3;
+
+        RoundResult result = runner.run(remainTurns);
+
+        assertThat(result.positions()).hasSize(3);
+
+        assertThat(result.positions())
+                .extracting(p -> p.name())
+                .containsExactlyInAnyOrder("pobi", "woni", "jun");
+
+        assertThat(result.positions())
+                .anySatisfy(p -> {
+                    if (p.name().equals("pobi")) {
+                        assertThat(p.position()).isEqualTo(pobi.getPosition());
+                    }
+                });
+
+        assertThat(result.positions())
+                .anySatisfy(p -> {
+                    if (p.name().equals("woni")) {
+                        assertThat(p.position()).isEqualTo(woni.getPosition());
+                    }
+                });
+
+        assertThat(result.positions())
+                .anySatisfy(p -> {
+                    if (p.name().equals("jun")) {
+                        assertThat(p.position()).isEqualTo(jun.getPosition());
+                    }
+                });
+    }
+}
