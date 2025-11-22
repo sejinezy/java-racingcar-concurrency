e773a3fb3def3126af6ea329d7a53c7527ae7d5a
sejin <psj8880@naver.com>
2025-11-22 12:21:43 +0900

    test(ApplicationTest): 테스트 작성
    - 기능 테스트 (무구,형식,우승자)
    - 예외 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,31 +3,45 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+
 
     @Test
     void 기능_테스트() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
-        );
+        assertSimpleTest(() -> {
+            run("pobi,woni", "1");
+            String output = output();
+
+            assertThat(output).contains(
+                    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+                    "시도할 횟수는 몇 회인가요?",
+                    "실행 결과"
+            );
+
+            assertThat(output).contains("pobi :");
+            assertThat(output).contains("woni :");
+
+            assertThat(output).contains("최종 우승자 : ");
+
+            assertThat(output).containsAnyOf(
+                    "최종 우승자 : pobi",
+                    "최종 우승자 : woni",
+                    "최종 우승자 : pobi, woni"
+            );
+
+        });
     }
 
     @Test
     void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+        assertSimpleTest(() ->{
+            run("pobi,javaji", "1");
+            assertThat(output())
+                    .contains("[ERROR] 이름은 5자 이하만 가능합니다.");
+                }
         );
     }
 
